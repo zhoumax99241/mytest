53e034db333043a0f50e2146cfa639f85e62f97e
chenqiusheng <qiusheng.chen@bl.com>
2018-06-07 14:12:36 +0800

    xx

--- mydemo-1/src/main/java/com/example/demo/Mydemo1Application.java
+++ mydemo-1/src/main/java/com/example/demo/Mydemo1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Mydemo1Application {
 
 	public static void main(String[] args) {
+		System.out.println("==========");
 		SpringApplication.run(Mydemo1Application.class, args);
 	}
 }
